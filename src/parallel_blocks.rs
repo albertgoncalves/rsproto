bf799f65d5fa15299c743cb8bf3eaa94bf17f8da
albertgoncalves <goncalves.albert@gmail.com>
2020-06-05 18:46:33 -0400

    no-ops

--- src/parallel_blocks.rs
+++ src/parallel_blocks.rs
@@ -3,11 +3,11 @@ use std::thread;
 use std::thread::JoinHandle;
 use std::time::Duration;
 
-const BUFFER_WIDTH: usize = 20;
-const BUFFER_HEIGHT: usize = 20;
+const BUFFER_WIDTH: usize = 17;
+const BUFFER_HEIGHT: usize = 17;
 const BUFFER_CAP: usize = BUFFER_WIDTH * BUFFER_HEIGHT;
 
-const N: usize = 3;
+const N: usize = 4;
 const BLOCK_WIDTH: usize = BUFFER_WIDTH / N;
 const BLOCK_HEIGHT: usize = BUFFER_HEIGHT / N;
 const X_BLOCKS: usize = (BUFFER_WIDTH + BLOCK_WIDTH - 1) / BLOCK_WIDTH;
@@ -23,7 +23,7 @@ struct XY {
 }
 
 #[derive(Clone, Copy, Debug)]
-struct Work {
+struct Block {
     start: XY,
     end: XY,
 }
@@ -40,33 +40,26 @@ unsafe impl<T> Sync for ThreadMutPtr<T> {}
 
 static INDEX: AtomicUsize = AtomicUsize::new(0);
 
-fn set_buffer(buffer: &mut [u8], works: &[Work]) {
-    loop {
-        let index: usize = INDEX.fetch_add(1, Ordering::SeqCst);
-        if BLOCKS_CAP <= index {
-            return;
+fn set_buffer(buffer: &mut [u8], block: Block, value: u8) {
+    for y in block.start.y..block.end.y {
+        let offset: usize = y * BUFFER_WIDTH;
+        for x in block.start.x..block.end.x {
+            buffer[x + offset] = value;
         }
-        let work: Work = works[index];
-        for y in work.start.y..work.end.y {
-            let offset: usize = y * BUFFER_WIDTH;
-            for x in work.start.x..work.end.x {
-                buffer[x + offset] = index as u8;
-            }
-        }
-        thread::sleep(Duration::from_millis(100))
     }
+    thread::sleep(Duration::from_millis(100))
 }
 
 fn main() {
     let mut buffer: [u8; BUFFER_CAP] = [0; BUFFER_CAP];
-    let mut works: Vec<Work> = Vec::with_capacity(BLOCKS_CAP);
+    let mut blocks: Vec<Block> = Vec::with_capacity(BLOCKS_CAP);
     for y in 0..Y_BLOCKS {
         for x in 0..X_BLOCKS {
             let start: XY = XY {
                 x: x * BLOCK_WIDTH,
                 y: y * BLOCK_HEIGHT,
             };
-            works.push(Work {
+            blocks.push(Block {
                 start,
                 end: XY {
                     x: (start.x + BLOCK_WIDTH).min(BUFFER_WIDTH),
@@ -78,10 +71,18 @@ fn main() {
     let mut handles: Vec<JoinHandle<()>> = Vec::with_capacity(THREAD_CAP);
     for _ in 0..THREAD_CAP {
         let buffer: ThreadMutPtr<[u8; BUFFER_CAP]> = ThreadMutPtr(&mut buffer);
-        let works: ThreadPtr<Vec<Work>> = ThreadPtr(&works);
+        let blocks: ThreadPtr<Vec<Block>> = ThreadPtr(&blocks);
         unsafe {
             handles.push(thread::spawn(move || {
-                set_buffer(&mut (*buffer.0), &(*works.0));
+                let buffer: &mut [u8] = &mut (*buffer.0);
+                let blocks: &Vec<Block> = &(*blocks.0);
+                loop {
+                    let index: usize = INDEX.fetch_add(1, Ordering::SeqCst);
+                    if BLOCKS_CAP <= index {
+                        return;
+                    }
+                    set_buffer(buffer, blocks[index], index as u8);
+                }
             }));
         }
     }
