a3638f03aa1178049e73295f72faf74c731749bb
albertgoncalves <goncalves.albert@gmail.com>
2024-04-17 22:30:21 -0400

    expand glfw ffi demo

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,8 +1,6 @@
 use std::ffi;
 use std::marker;
 use std::ptr;
-use std::thread;
-use std::time;
 
 macro_rules! opaque_struct {
     ($name:ident) => {
@@ -18,10 +16,14 @@ macro_rules! opaque_struct {
 opaque_struct!(GLFWwindow);
 opaque_struct!(GLFWmonitor);
 
+type GLFWkeyfun = extern "C" fn(*mut GLFWwindow, ffi::c_int, ffi::c_int, ffi::c_int, ffi::c_int);
+
 extern "C" {
     fn glfwGetVersionString() -> *const ffi::c_char;
+
     fn glfwInit() -> ffi::c_int;
     fn glfwTerminate();
+
     fn glfwCreateWindow(
         width: ffi::c_int,
         height: ffi::c_int,
@@ -30,6 +32,33 @@ extern "C" {
         share: *mut GLFWwindow,
     ) -> *mut GLFWwindow;
     fn glfwDestroyWindow(window: *mut GLFWwindow);
+
+    fn glfwSetKeyCallback(window: *mut GLFWwindow, callback: GLFWkeyfun) -> GLFWkeyfun;
+    fn glfwSetWindowShouldClose(window: *mut GLFWwindow, value: ffi::c_int);
+
+    fn glfwMakeContextCurrent(window: *mut GLFWwindow);
+    fn glfwSwapInterval(interval: ffi::c_int);
+
+    fn glfwWindowShouldClose(window: *mut GLFWwindow) -> ffi::c_int;
+    fn glfwPollEvents();
+}
+
+const GLFW_PRESS: ffi::c_int = 1;
+const GLFW_KEY_ESCAPE: ffi::c_int = 256;
+
+extern "C" fn callback_key(
+    window: *mut GLFWwindow,
+    key: ffi::c_int,
+    _scancode: ffi::c_int,
+    action: ffi::c_int,
+    _mods: ffi::c_int,
+) {
+    if action != GLFW_PRESS {
+        return;
+    }
+    if key == GLFW_KEY_ESCAPE {
+        unsafe { glfwSetWindowShouldClose(window, 1) }
+    }
 }
 
 fn main() {
@@ -37,7 +66,8 @@ fn main() {
         let version = ffi::CStr::from_ptr(glfwGetVersionString())
             .to_str()
             .unwrap();
-        println!("{version}\n{}", glfwInit());
+        println!("{version}");
+        assert!(glfwInit() == 1);
         let window = glfwCreateWindow(
             1536,
             768,
@@ -49,7 +79,16 @@ fn main() {
             ptr::null_mut::<GLFWmonitor>(),
             ptr::null_mut::<GLFWwindow>(),
         );
-        thread::sleep(time::Duration::from_millis(2500));
+
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+
+        glfwSetKeyCallback(window, callback_key);
+
+        while glfwWindowShouldClose(window) != 1 {
+            glfwPollEvents();
+        }
+
         glfwDestroyWindow(window);
         glfwTerminate();
     }
