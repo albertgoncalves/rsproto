abf2f5f5e8450b95ccef31a2f2290f9f2ae2e5fb
albertgoncalves <goncalves.albert@gmail.com>
2024-04-30 22:25:42 -0400

    continue to flesh out FFI demo

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,4 +1,3 @@
-use std::convert::TryInto;
 use std::ffi::{c_char, c_int, CStr, CString};
 use std::marker;
 use std::ptr;
@@ -18,6 +17,7 @@ macro_rules! opaque_struct {
 opaque_struct!(GLFWwindow);
 opaque_struct!(GLFWmonitor);
 
+type GLFWerrorfun = extern "C" fn(error_code: c_int, description: *const c_char);
 type GLFWkeyfun = extern "C" fn(*mut GLFWwindow, c_int, c_int, c_int, c_int);
 
 extern "C" {
@@ -35,6 +35,8 @@ extern "C" {
     ) -> *mut GLFWwindow;
     fn glfwDestroyWindow(window: *mut GLFWwindow);
 
+    fn glfwSetErrorCallback(callback: GLFWerrorfun) -> GLFWerrorfun;
+
     fn glfwSetKeyCallback(window: *mut GLFWwindow, callback: GLFWkeyfun) -> GLFWkeyfun;
     fn glfwSetWindowShouldClose(window: *mut GLFWwindow, value: c_int);
 
@@ -44,87 +46,50 @@ extern "C" {
     fn glfwWindowShouldClose(window: *mut GLFWwindow) -> c_int;
     fn glfwPollEvents();
     fn glfwSwapBuffers(window: *mut GLFWwindow);
+
+    fn glEnable(cap: u32);
+    fn glBlendFunc(sfactor: u32, dfactor: u32);
+
+    fn glClearColor(red: f32, green: f32, blue: f32, alpha: f32);
+    fn glClear(mask: u32);
 }
 
 const GLFW_PRESS: c_int = 1;
 const GLFW_KEY_ESCAPE: c_int = 256;
 
-struct Glfw {
-    window: *mut GLFWwindow,
-}
+const GL_COLOR_BUFFER_BIT: u32 = 0x0000_4000;
+const GL_DEPTH_BUFFER_BIT: u32 = 0x0000_0100;
+const GL_STENCIL_BUFFER_BIT: u32 = 0x0000_0400;
 
-impl Glfw {
-    fn new(width: u32, height: u32, callback_key: GLFWkeyfun) -> Self {
-        let version = unsafe { CStr::from_ptr(glfwGetVersionString()) }
-            .to_str()
-            .unwrap();
-        println!("{version}");
+const GL_BLEND: u32 = 0x0BE2;
+const GL_SRC_ALPHA: u32 = 0x0302;
+const GL_ONE_MINUS_SRC_ALPHA: u32 = 0x0303;
 
-        assert!(unsafe { glfwInit() } == 1);
+struct Defer<F: FnMut()>(F);
 
-        let window = unsafe {
-            glfwCreateWindow(
-                width.try_into().unwrap(),
-                height.try_into().unwrap(),
-                CString::new(std::module_path!())
-                    .unwrap()
-                    .as_bytes_with_nul()
-                    .as_ptr()
-                    .cast::<c_char>(),
-                ptr::null_mut::<GLFWmonitor>(),
-                ptr::null_mut::<GLFWwindow>(),
-            )
-        };
-        if window.is_null() {
-            unsafe { glfwTerminate() };
-            panic!();
-        }
-
-        unsafe {
-            glfwMakeContextCurrent(window);
-            glfwSwapInterval(1);
-            glfwSetKeyCallback(window, callback_key);
-        }
-
-        Self { window }
-    }
-
-    fn r#loop<T>(&mut self, state: &mut T, update: fn(&mut T), draw: fn(&T)) {
-        let mut now = time::Instant::now();
-        let mut frames = 0;
-
-        println!("\n");
-        while unsafe { glfwWindowShouldClose(self.window) } != 1 {
-            if 0 < now.elapsed().as_secs() {
-                println!(
-                    "\x1B[2A\
-                     {:10} ns/f\n\
-                     {frames:10} frames",
-                    now.elapsed().as_nanos() / frames,
-                );
-                now = time::Instant::now();
-                frames = 0;
-            }
-
-            unsafe { glfwPollEvents() };
-
-            update(state);
-            draw(state);
-
-            unsafe { glfwSwapBuffers(self.window) };
-
-            frames += 1;
-        }
-    }
-}
-
-impl Drop for Glfw {
+impl<F: FnMut()> Drop for Defer<F> {
     fn drop(&mut self) {
-        unsafe {
-            glfwDestroyWindow(self.window);
-            glfwTerminate();
-        }
+        (self.0)();
     }
+}
+
+macro_rules! defer {
+    ($expr:expr) => {
+        let _defer = Defer(|| {
+            $expr;
+        });
+    };
+}
+
+extern "C" fn callback_error(error_code: c_int, description: *const c_char) {
+    let mut message = error_code.to_string();
+    if !description.is_null() {
+        message.push_str(&format!(
+            ": {}",
+            unsafe { CStr::from_ptr(description) }.to_str().unwrap(),
+        ));
+    }
+    panic!("{}", message);
 }
 
 extern "C" fn callback_key(
@@ -142,14 +107,62 @@ extern "C" fn callback_key(
     }
 }
 
-struct State {}
-
-fn update(_state: &mut State) {}
-
-const fn draw(_state: &State) {}
-
 fn main() {
-    let mut glfw = Glfw::new(1536, 768, callback_key);
-    let mut state = State {};
-    glfw.r#loop(&mut state, update, draw);
+    unsafe {
+        let version = CStr::from_ptr(glfwGetVersionString()).to_str().unwrap();
+        println!("{version}");
+
+        assert!(glfwInit() == 1);
+        defer!(glfwTerminate());
+
+        glfwSetErrorCallback(callback_error);
+
+        let window = glfwCreateWindow(
+            1536,
+            768,
+            CString::new(std::module_path!())
+                .unwrap()
+                .as_bytes_with_nul()
+                .as_ptr()
+                .cast::<c_char>(),
+            ptr::null_mut::<GLFWmonitor>(),
+            ptr::null_mut::<GLFWwindow>(),
+        );
+        assert!(!window.is_null());
+        defer!(glfwDestroyWindow(window));
+
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+        glfwSetKeyCallback(window, callback_key);
+
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glClearColor(0.1, 0.1, 0.1, 1.0);
+
+        let mut now = time::Instant::now();
+        let mut frames = 0;
+
+        println!("\n");
+
+        while glfwWindowShouldClose(window) != 1 {
+            if 0 < now.elapsed().as_secs() {
+                println!(
+                    "\x1B[2A\
+                     {:10} ns/f\n\
+                     {frames:10} frames",
+                    now.elapsed().as_nanos() / frames,
+                );
+                now = time::Instant::now();
+                frames = 0;
+            }
+
+            glfwPollEvents();
+
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(window);
+
+            frames += 1;
+        }
+    }
 }
