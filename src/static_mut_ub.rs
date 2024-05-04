32af9712e5dc9c3927b91ba457243679a37c4757
albertgoncalves <goncalves.albert@gmail.com>
2024-05-04 11:58:32 -0400

    demo UB with `static mut`s

---
+++ src/static_mut_ub.rs
@@ -0,0 +1,20 @@
+static mut EXAMPLE: i32 = 0;
+
+fn example(ref1: &mut i32, ref2: &mut i32) -> i32 {
+    *ref1 = 1;
+    *ref2 = 2;
+    *ref1
+}
+
+fn main() {
+    use std::hint::black_box;
+    let ref1 = black_box(unsafe {
+        #[allow(static_mut_refs)]
+        &mut EXAMPLE
+    });
+    let ref2 = black_box(unsafe {
+        #[allow(static_mut_refs)]
+        &mut EXAMPLE
+    });
+    println!("{}", example(ref1, ref2));
+}
